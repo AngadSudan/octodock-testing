a30358adbc1b3521764a01f4576bb75d8d9ff1fa
Angad Sudan <angadsudan453@gmail.com>
2025-07-13 14:48:03 +0530

    Octodock commit for your backend success

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, StackBlitz!</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Card() {
+  return <div className="card">This is a card component</div>;
+}
